a06e0c9e78d8088a85e750ba9be20dcbe50a09eb
bloodyfork <mohtor.138055@gmail.com>
2024-05-09 21:53:59 +0430

    created user model and basemodel

--- core/models.py
+++ core/models.py
@@ -1,3 +1,42 @@
+from django.contrib.auth.models import UserManager
 from django.db import models
 
+
 # Create your models here.
+class BaseManager(models.Manager):
+
+    def get_queryset(self):
+        return super().get_queryset().filter(is_deleted=False)
+
+    def archive(self):
+        return super().get_queryset()
+
+
+class BaseModel(models.Model):
+    class Meta:
+        abstract = True
+
+    is_deleted = models.BooleanField(default=False)
+    create_timestamp = models.DateTimeField(auto_now_add=True)
+    modify_timestamp = models.DateTimeField(auto_now=True)
+    delete_timestamp = models.DateTimeField(default=None, null=True, blank=True)
+
+    objects = BaseManager()
+
+    def logical_delete(self):
+        self.is_deleted = True
+
+
+class MyUserManager(UserManager):
+
+    # def _create_user(self, username, email, password, **extra_fields):
+    #     return super()._create_user(username, email, password, **extra_fields)
+
+    def create_superuser(self, username=None, email=None, password=None, **extra_fields):
+        username = extra_fields['student_number']
+        return super().create_superuser(username, email, password, **extra_fields)
+
+    def create_user(self, username=None, email=None, password=None, **extra_fields):
+        username = extra_fields['student_number']
+        return super().create_user(username, email, password, **extra_fields)
+
--- خوابگاه/models.py
+++ خوابگاه/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from  core.models import MyUserManager
 # Create your models here.
+class User(AbstractUser):
+    objects = MyUserManager()
+    student_number = models.CharField(max_length=13, unique=True, verbose_name='شماره دانشجویی')
+    USERNAME_FIELD = 'student_number'
+
+    def save(self, *args, **kwargs):
+        self.username = self.student_number
+        if User.objects.filter(id=self.id):
+            self.set_password(self.password)
+        super().save(*args, **kwargs)
