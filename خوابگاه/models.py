5202b4fe226ad4d34dc1d438f10ad6ea5c4fd880
bloodyfork <mohtor.138055@gmail.com>
2024-05-09 22:00:43 +0430

    added room model

--- خوابگاه/models.py
+++ خوابگاه/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from  core.models import MyUserManager
+from core.models import MyUserManager
+from core.models import BaseModel
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
 # Create your models here.
 class User(AbstractUser):
     objects = MyUserManager()
@@ -12,3 +16,29 @@ class User(AbstractUser):
         if User.objects.filter(id=self.id):
             self.set_password(self.password)
         super().save(*args, **kwargs)
+
+
+class Room(BaseModel):
+    room_number = models.PositiveIntegerField(verbose_name='شماره ی اتاق', unique=True, blank=False,
+                                              validators=[MinValueValidator(1), MaxValueValidator(300)],
+                                              help_text='شماره ی اتاق')
+    capacity = models.PositiveIntegerField(verbose_name='ظرفیت اتاق', blank=False, default=5,
+                                           validators=[MinValueValidator(1), MaxValueValidator(8)],
+                                           help_text='ظرفیت اتاق')
+    corridor = models.CharField(verbose_name='نام راهرو', max_length=40,
+                                help_text='نام راهرویی که اتاق در آن قرار دارد')
+    floor = models.PositiveSmallIntegerField(verbose_name='طبقه',
+                                             validators=[MinValueValidator(1), MaxValueValidator(4)],
+                                             help_text='طبقه ای که اتاق در آن قرار دارد')
+    DORM_CHOICES = (
+        ('پسران', 'پسران'),
+        ('دختران', 'دختران'),
+    )
+    dorm = models.CharField(verbose_name='خوابگاه', choices=DORM_CHOICES, max_length=7,
+                            help_text='اتاق در خوابگاه دختران است یا پسران')
+    is_full = models.BooleanField(verbose_name='تکمیل ظرفیت', default=False, blank=False,
+                                  help_text='آیا ظرفیت اتاق تکمیل است؟(تیک سبز یعنی بله)')
+
+    class Meta:
+        verbose_name = 'اتاق'
+        verbose_name_plural = 'اتاق ها'
