c3258e1f20ce67c9d438a6fa018dd57ae49c3ede
bloodyfork <mohtor.138055@gmail.com>
2024-05-11 22:55:23 +0430

    added login landing

--- core/views.py
+++ core/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
+from django.contrib import messages
+from خوابگاه.models import User
 # Create your views here.
+
+
+class LoginStudent(LoginView):
+    template_name = "home/login.html"
+
+    def post(self, request, *args, **kwargs):
+
+        username = request.POST.get('phone')
+        password = request.POST.get('password')
+        user = authenticate(request, phone=username, password=password)
+        if user is not None:
+            User.objects.get_or_create(user=user)
+            login(request, user)
+            return redirect(to='home')
+
+        else:
+            messages.info(request, "incorrect Password or Phone number")
+            return render(request, 'home/login.html')
--- khaabgaahh/urls.py
+++ khaabgaahh/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core.views import LoginStudent
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', LoginStudent.as_view(), name='student_login')
 ]
